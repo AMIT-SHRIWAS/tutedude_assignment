a01dfd1eb05a671099d1a09371ebb8afc1c710a0
AMIT-SHRIWAS <ashriwas702@gmil.com>
2025-12-28 19:23:17 +0530

    initial

--- Assignment_3/Task_2.py
+++ Assignment_3/Task_2.py
@@ -1,5 +1,5 @@
-import math
+from math import sqrt, log, sin
 number = int(input('Enter a number: '))
-print(f'Square root :{math.sqrt(number)}')
-print(f'Logrithm: {math.log(number, 2)}')
-print(f'sine: {math.sin(number)}')
+print(f'Square root :{sqrt(number)}')
+print(f'Logrithm: {log(number, 2)}')
+print(f'sine: {sin(number)}')
